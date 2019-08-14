1076be2c7baa9a134b25acbf943f8af72e94ce20
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-14 20:34:57 -0300

    ADD Button Login component

---
+++ src/components/Buttons/Login/ButtonLogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ButtonLogin.css'
+
+export default props => {
+
+    return (
+        <div className="form-group text-center">
+            <input type="submit" name="submit" className="btn btn-primary btn-md" value="Entrar" />
+        </div>
+    )
+}
+   
