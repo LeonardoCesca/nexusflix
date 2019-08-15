a3f4e4dfa8657dbf9fa66550fc4566f519896fbf
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-14 22:44:23 -0300

    UPDATE login with validation login and pass (simple)

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 import './Login.css'
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
 
-    constructor() {
-        super();
         this.state = {
             username: '',
             password: '',
+            verify: false,
         };
     
         this.handleUserName = this.handleUserName.bind(this)
+        this.handlePassword = this.handlePassword.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     
     }
 
@@ -18,23 +21,40 @@ export default class Login extends Component {
         this.setState({
             username: event.target.value
         })
+        if (this.username === 'admin') {
+            return true
+        }
     } 
-    
+
+    handlePassword(event) {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        if(this.state.password === 'admin' && this.state.username === 'admin') {
+            console.log('foi')
+        }else{
+            console.log('no')
+        }
+        event.preventDefault()
+    }
 
 
     render() {
         return (
             <div className="login">
                 <h1 className="login-title">Login</h1>
-                <form className="form">
+                <form className="form" onSubmit={this.handleSubmit}>
 
                 <label className="form-label">Login</label>
-                <input className="form-input" type="text" value={this.state.username} onChange={this.handleUserName}/>
+                <input className="form-input" type="text" value={this.state.username} onChange={this.handleUserName} />
 
                 <label className="form-label">Senha</label>
-                <input className="form-input" type="password" />
+                <input className="form-input" type="password" value={this.state.password} onChange={this.handlePassword} />
         
-                <input type="submit" value="Log In" />
+                <input type="submit" />
                 </form>
             </div>
         )
