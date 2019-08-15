5a53f6b88807673c0031e147292f53a2289cfbe6
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-14 21:03:06 -0300

    DELETE bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './Login/Login';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<Login />, document.getElementById('root'));
 
