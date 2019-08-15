3771463ca04e1d11be33de7067f81953f3c4adfd
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-14 21:38:44 -0300

    UPDATE Login component

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,27 +1,41 @@
 import React, { Component } from 'react'
 import './Login.css'
 
-import InputLogin from '../components/Inputs/Login/InputLogin'
-import ButtonLogin from '../components/Buttons/Login/ButtonLogin'
-
-
 export default class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+        };
+    
+        this.handleUserName = this.handleUserName.bind(this)
+    
+    }
+
+    handleUserName(event) {
+        this.setState({
+            username: event.target.value
+        })
+    } 
+    
+
+
     render() {
         return (
-            <div id="login" className="d-flex justify-content-center align-items-center">
-                <div className="container">
-                    <div id="login-row" className="row justify-content-center align-items-center">
-                        <div id="login-column" className="col-md-6">
-                            <div id="login-box" className="col-md-12">
-                                <form id="login-form" className="form" action="" method="post">
-                                    <h3 className="text-center text-info">Login</h3>
-                                    <InputLogin />
-                                    <ButtonLogin />
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="login">
+                <h1 className="login-title">Login</h1>
+                <form className="form">
+
+                <label className="form-label">Login</label>
+                <input className="form-input" type="text" value={this.state.username} onChange={this.handleUserName}/>
+
+                <label className="form-label">Senha</label>
+                <input className="form-input" type="password" />
+        
+                <input type="submit" value="Log In" />
+                </form>
             </div>
         )
     }
