65447a9b5c5a91bfbdc7184737808e4ae74bcff1
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-15 21:50:16 -0300

    ADD new route - Home component

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./Login/Login";
+import Home from "./components/Home"
 
 const Routes = () => (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/home" component={() => <h1>SignUp</h1>} />
+        <Route path="/home" component={ Home } />
         <Route path="*" component={() => <h1>Page not found</h1>} />
       </Switch>
     </BrowserRouter>
