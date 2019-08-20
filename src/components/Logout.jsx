30a2c6d41e1cd80cf6190fb3c93753ac8c8fe65c
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-19 22:39:15 -0300

    ADD styling for Logout component

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import emoji from '../assets/img/emoji.png'
+import './Logout.css'
 
 export default class Logout extends Component {
     constructor(props) {
@@ -8,9 +10,13 @@ export default class Logout extends Component {
     }
     render() {
         return (
-            <div>
-                <h1>Logout</h1>
+            <div className='logout'>
+                <img src={ emoji } alt="" className='logout__img' />
+                <h1 className='logout__title'>Você saiu.</h1>
+                <span className='logout__text'> Espero ver você em breve.</span>
+                <div className='logout__link'>
                 <Link to="/">Login</Link>
+                </div>
             </div>
         )
     }
