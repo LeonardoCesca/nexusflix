a8d092fe28e64a6d0f784ec88d94d63f16854170
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-20 15:36:54 -0300

    ADD ajusts - media query

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -10,7 +10,7 @@ export default class Play extends Component {
             <h1 className='play__title'>Play</h1>
             <Link to="/Home" className="play__back">Voltar</Link>
             <div className='play__iframe'>  
-                <iframe title='movie' width="700" height="500" src="https://www.youtube.com/embed/RSfxQBOY0qA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe title='movie' width="700" height="500" src="https://www.youtube.com/embed/RSfxQBOY0qA" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
             </div>
             </div>
         )
